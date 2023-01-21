1c651b86d49de0205410acf343444c58da596f9d
Monson2002 <monsonreji2002@gmail.com>
2023-01-21 10:26:29 +0530

    21-1-23 10:26am Refactored navbar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import { TypeAnimation } from 'react-type-animation';
 
 export default function Home() {
@@ -14,10 +14,23 @@ export default function Home() {
         }
     }
 
+    // const [navColor, setnavColor] = useState("bg-gray-700");
+    // const listenScrollEvent = () => {
+    //     window.scrollY > 619 && window.scrollY < 1837 ? setnavColor("bg-gray-800") : setnavColor("bg-gray-700");
+    //     window.scrollY > 1837 ? setnavColor("bg-gray-700") : setnavColor("bg-gray-800");
+    //     // window.scrollY > 619 ? setnavColor("bg-gray-800") : setnavColor("bg-gray-700");
+    // };
+    // useEffect(() => {
+    //     window.addEventListener("scroll", listenScrollEvent);
+    //     return () => {
+    //         window.removeEventListener("scroll", listenScrollEvent);
+    //     };
+    // }, []);
+
     return (
         <>
             <main className='home h-screen w-screen bg-gray-800 flex flex-col justify-between items-center' id='home'>
-                <div className='navbar-toggle fixed flex justify-between items-center w-full p-2 bg-gray-700 z-10'>
+                <div className={`navbar-toggle fixed flex justify-between items-center w-full p-2 bg-gray-700 z-10`}>
                     <div className='logo flex justify-center items-center'>
                         <a href="https://www.linkedin.com/in/monson-reji-verghese-a26061231/" target="_blank" rel="noopener noreferrer">
                             <i className="fa-brands fa-linkedin text-slate-300 active:text-slate-400 scale-150 sm:scale-[200%] lg:scale-[300%] m-2 sm:m-3 lg:m-6 hover:text-slate-400 hover:rotate-360 transition-all duration-1000"></i>
@@ -30,10 +43,14 @@ export default function Home() {
                     <div className='dropdown-md hidden md:flex'>
                         <div className='dropdown'>
                             <ul className="text-gray-300 flex justify-center items-center md:mx-2 rounded-lg">
-                                <a href="#home"><li className="p-2 m-1 w-auto text-lg font-normal hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:bg-slate-800 active:text-slate-100" onClick={toggleNavbar}>Home</li></a>
-                                <a href="#about"><li className="p-2 m-1 w-auto text-lg font-normal hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:bg-slate-800 active:text-slate-100" onClick={toggleNavbar}>About</li></a>
-                                <a href="#projects"><li className="p-2 m-1 w-auto text-lg font-normal hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:bg-slate-800 active:text-slate-100" onClick={toggleNavbar}> Projects</li></a>
-                                <a href="#contact-us"><li className="p-2 m-1 w-auto text-lg font-normal hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:bg-slate-800 active:text-slate-100" onClick={toggleNavbar}> Contact</li></a>
+                                <a href="#home"><i className="fa-solid fa-house p-2 m-2 sm:m-4 lg:mx-8 w-auto md:scale-150 hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:text-slate-100"></i></a>
+                                {/* <li className="p-2 m-1 w-auto text-lg font-normal hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:bg-slate-800 active:text-slate-100">Home</li> */}
+                                <a href="#about"><i className="fa-solid fa-user p-2 m-2 sm:m-4 lg:mx-8 w-auto md:scale-150 hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:text-slate-100"></i></a>
+                                {/* <li className="p-2 m-1 w-auto text-lg font-normal hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:bg-slate-800 active:text-slate-100">About</li> */}
+                                <a href="#projects"><i className="fa-solid fa-briefcase p-2 m-2 sm:m-4 lg:mx-8 w-auto md:scale-150 hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:text-slate-100"></i></a>
+                                {/* <li className="p-2 m-1 w-auto text-lg font-normal hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:bg-slate-800 active:text-slate-100"> Projects</li> */}
+                                <a href="#contact-us"><i className="fa-solid fa-phone p-2 m-2 sm:m-4 lg:mx-8 w-auto md:scale-150 hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:text-slate-100"></i></a>
+                                {/* <li className="p-2 m-1 w-auto text-lg font-normal hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:bg-slate-800 active:text-slate-100"> Contact</li> */}
                             </ul>
                         </div>
                     </div>
@@ -43,10 +60,22 @@ export default function Home() {
                         {navBar ?
                             (<div className='dropdown'>
                                 <ul className="absolute m-3 mt-4 left-0 w-11/12 bg-slate-900 text-gray-300 flex flex-col justify-start items-center p-2 sm:p-3 rounded-lg">
-                                    <a href="#home"><li className="p-2 m-1 w-auto text-lg font-normal active:bg-slate-800 active:text-slate-100" onClick={toggleNavbar}>Home</li></a>
-                                    <a href="#about"><li className="p-2 m-1 w-auto text-lg font-normal active:bg-slate-800 active:text-slate-100" onClick={toggleNavbar}>About</li></a>
-                                    <a href="#projects"><li className="p-2 m-1 w-auto text-lg font-normal active:bg-slate-800 active:text-slate-100" onClick={toggleNavbar}>Projects</li></a>
-                                    <a href="#contact-us"><li className="p-2 m-1 w-auto text-lg font-normal active:bg-slate-800 active:text-slate-100" onClick={toggleNavbar}>Contact</li></a>
+                                    <a href="#home" className='flex hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:bg-slate-800 active:text-slate-100'>
+                                        <i className="fa-solid fa-house m-auto p-2 w-auto "></i>
+                                        <li className="p-2 w-auto text-lg font-normal">Home</li>
+                                    </a>
+                                    <a href="#about" className='flex hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:bg-slate-800 active:text-slate-100'>
+                                        <i className="fa-solid fa-user m-auto p-2 w-auto "></i>
+                                        <li className="p-2 w-auto text-lg font-normal">About</li>
+                                    </a>
+                                    <a href="#projects" className='flex hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:bg-slate-800 active:text-slate-100'>
+                                        <i className="fa-solid fa-briefcase m-auto p-2 w-auto "></i>
+                                        <li className="p-2 w-auto text-lg font-normal">Projects</li>
+                                    </a>
+                                    <a href="#contacy-us" className='flex hover:text-slate-400 hover:underline hover:underline-offset-4 hover:decoration-pink-500 active:bg-slate-800 active:text-slate-100'>
+                                        <i className="fa-solid fa-phone m-auto p-2 w-auto "></i>
+                                        <li className="p-2 w-auto text-lg font-normal">Contact</li>
+                                    </a>
                                 </ul>
                             </div>)
                             :
@@ -70,17 +99,17 @@ export default function Home() {
                         <TypeAnimation
                             sequence={[
                                 'React',
-                                1500, // Waits 2s
+                                1500, // Waits 1.5s
                                 'Python',
-                                1500, // Waits 2s
+                                1500, // Waits 1.5s
                                 'Java', // 
-                                1500, // Waits 2s
+                                1500, // Waits 1.5s
                                 'JavaScript',
-                                1500, // Waits 2s
+                                1500, // Waits 1.5s
                                 'Tailwind CSS',
-                                1500, // Waits 2s
+                                1500, // Waits 1.5s
                                 'HTML',
-                                1500, // Waits 2s
+                                1500, // Waits 1.5s
                                 'Github',
                                 1500,
                             ]}
@@ -102,7 +131,7 @@ export default function Home() {
                 </div>
 
                 <div className='end'>
-                    {/* Dummy div for nertical alignment */}
+                    {/* Dummy div for vertical alignment */}
                 </div>
             </main>
         </>

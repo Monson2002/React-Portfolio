eaf709a1422b257aaa943f73ce5416bb8276fd8a
dipak <dipakja01@gmail.com>
2023-05-19 22:37:24 +0530

    i change the hover color on resume button and contact page box border

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -59,24 +59,24 @@ export default function ContactUs() {
                 theme="dark"
             />
 
-            <div className='flex justify-center items-center w-10/12 sm:m-1'>
-                <label className='text-gray-300 font-CustomFont1 p-2  '>
+            <div className='flex justify-center items-center w-10/12 sm:m-1 '>
+                <label className='text-gray-300 font-CustomFont1 p-2   '>
                     Name
                 </label>
-                <input required className='p-1 rounded text-slate-800 text-sm w-10/12' type="text" name="user_name" />
+                <input required className='p-1 rounded text-slate-800 text-sm w-10/12 group-hover:border-solid border-2 border-sky-500   ' type="text" name="user_name" />
             </div>
-            <div className='flex justify-center items-center w-10/12 sm:m-1'>
+            <div className='flex justify-center items-center w-10/12 sm:m-1 '>
                 <label className='text-gray-300 font-CustomFont1 p-2  '>
                     Email
                 </label>
-                <input required className='p-1 rounded text-slate-800 text-sm w-10/12' type="email" name="user_email" />
+                <input required className='p-1 rounded text-slate-800 text-sm w-10/12 group-hover:border-solid border-2 border-sky-500' type="email" name="user_email" />
             </div>
             <div className='flex flex-col justify-center items-stretch w-full sm:m-1 sm:w-3/4'>
                 <label className='text-gray-300 font-CustomFont1 p-2 '>
                     Message :
                 </label>
                 <div className='m-auto flex justify-center items-center w-full sm:w-full'>
-                    <textarea required className='p-1 rounded text-slate-800 text-sm w-10/12 sm:w-full' name="message" rows={5} />
+                    <textarea required className='p-1 rounded text-slate-800 text-sm w-10/12 sm:w-full group-hover:border-solid border-2 border-sky-500' name="message" rows={5} />
                 </div>
             </div>
             <div className='flex justify-center items-center sm:m-2'>
